f3c89a29ef3ce5c986e3a3e3d33a2563b22aee02
majidhemmati1985 <92200986+majidhemmati1985@users.noreply.github.com>
2022-02-22 00:07:04 +0330

    Create put_text_in_video.py

---
+++ put_text_in_video.py
@@ -0,0 +1,33 @@
+import cv2
+import datetime
+
+cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+#fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
+#out = cv2.VideoWriter("output.avi",fourcc,20,(1280,528))
+#set width video
+
+
+# it must be noted that original size of video must be written correctly because it is not shown in video player
+while(cap.isOpened()):
+    ret,frame = cap.read()
+    if ret == True:
+        print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        datet = str(datetime.datetime.now())
+        frame = cv2.putText(frame,datet,(200,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0),2,cv2.LINE_AA)
+
+
+        cv2.imshow("Test",frame)
+
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
+
+
+
+
