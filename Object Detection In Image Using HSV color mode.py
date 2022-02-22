6b3aea065b6bf8c9fa9c798cee346aae82c85b52
majidhemmati1985 <92200986+majidhemmati1985@users.noreply.github.com>
2022-02-22 20:40:55 +0330

    Create Object Detection In Image Using HSV color mode.py

---
+++ Object Detection In Image Using HSV color mode.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+
+def nothing(x):
+    pass
+
+cv2.namedWindow("Tracking")
+cv2.createTrackbar("LH","Tracking",0,255,nothing)
+cv2.createTrackbar("LS","Tracking",0,255,nothing)
+cv2.createTrackbar("LV","Tracking",0,255,nothing)
+cv2.createTrackbar("UH","Tracking",255,255,nothing)
+cv2.createTrackbar("US","Tracking",255,255,nothing)
+cv2.createTrackbar("UV","Tracking",255,255,nothing)
+
+
+while True:
+    frame = cv2.imread("c:/samples/smarties.png")
+
+    hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+
+    l_h = cv2.getTrackbarPos("LH","Tracking")
+    l_S = cv2.getTrackbarPos("LS","Tracking")
+    l_v = cv2.getTrackbarPos("LV","Tracking")
+
+    u_h = cv2.getTrackbarPos("UH", "Tracking")
+    u_S = cv2.getTrackbarPos("US", "Tracking")
+    u_v = cv2.getTrackbarPos("UV", "Tracking")
+
+    l_b = np.array([l_h,l_S,l_v])
+    u_b = np.array([u_h, u_S, u_v])
+
+    mask = cv2.inRange(hsv,l_b,u_b)
+
+    res = cv2.bitwise_and(frame,frame,mask =mask)
+
+    cv2.imshow("frame", frame)
+    cv2.imshow("mask", mask)
+    cv2.imshow("res",res)
+
+
+    if cv2.waitKey(1) == 27:
+        break
+
+cv2.imshow()
